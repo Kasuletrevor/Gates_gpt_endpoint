8b55efc8fba225f251a2fa159ded50e21b0740f1
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2024-04-27 20:03:58 +0300

    modify to deploy successfully

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ with st.sidebar:
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
