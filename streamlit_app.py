ba510db325b4b3e7a5bc167229e71f9d12ba4792
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2024-04-27 17:25:49 +0300

    modify script

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,32 @@ import json
 # Streamlit app title
 st.title("GatesGPT API Query Tool")
 
+
+# language_options = ["English", "Luganda"]
+language = "English"
+    # category_options = ["Animal", "Crop"]
+    # category = st.selectbox("Category", options=category_options)
+category = ""
+topic = ""
+sub_topic = ""
+sub_category = ""
+location= ""
+    # topic = st.text_input("Topic", value="Fertilization")
+    # sub_topic = st.text_input("Sub Topic", value="")
+    # location = st.text_input("Location", value="")
+
 # Form to input the question and other parameters
 with st.form("query_form"):
+    # user_id = 315
     question = st.text_input("Question", value="What is the best fertilizer for wheat?")
-    language_options = ["English", "Luganda"]
-    language = st.selectbox("Language", options=language_options, index=0)
-    category_options = ["Animal", "Crop"]
-    category = st.selectbox("Category", options=category_options)
-    sub_category = st.text_input("Sub Category", value="")
-    topic = st.text_input("Topic", value="Fertilization")
-    sub_topic = st.text_input("Sub Topic", value="")
-    location = st.text_input("Location", value="")
+    # language_options = ["English", "Luganda"]
+    # language = st.selectbox("Language", options=language_options, index=0)
+    # category_options = ["Animal", "Crop"]
+    # category = st.selectbox("Category", options=category_options)
+    # sub_category = st.text_input("Sub Category", value="")
+    # topic = st.text_input("Topic", value="Fertilization")
+    # sub_topic = st.text_input("Sub Topic", value="")
+    # location = st.text_input("Location", value="")
     submitted = st.form_submit_button("Submit")
 
 if submitted:
@@ -31,6 +46,7 @@ if submitted:
     
     # Request data
     data = {
+        "user_id": "415",
         "question": question,
         "language": language,
         "category": category,
